1609a1439bb7db228d04323b62f07007c59d8f0c
Ahmed Khalaf <pxlshpr@gmail.com>
2022-02-25 10:54:25 +0500

    tag increment

---
+++ Sources/SwiftSugar/String+Markdown.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+extension String {
+    func toMarkdown() -> AttributedString {
+        do {
+            return try AttributedString(
+                markdown: self,
+                options: AttributedString.MarkdownParsingOptions(interpretedSyntax: .inlineOnlyPreservingWhitespace)
+            )
+        } catch {
+            print("Error parsing Markdown for string \(self): \(error)")
+            return AttributedString(self)
+        }
+    }
+}
