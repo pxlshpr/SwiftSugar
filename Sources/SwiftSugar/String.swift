5c02cafdbb2b58afe59b899ca191fe0e907c7708
Ahmed Khalaf <pxlshpr@gmail.com>
2022-02-25 10:44:17 +0500

    tag increment

--- Sources/SwiftSugar/String.swift
+++ Sources/SwiftSugar/String.swift
@@ -2,35 +2,35 @@ import Foundation
 
 public extension String {
     
-    var htmlContentsFromUrlString: String {
+    var htmlContentsFromUrlString: String? {
         guard let url = URL(string: self) else {
             print("Error: Couldn't form URL from string: \(self)")
-            return ""
+            return nil
         }
         do {
             let contents = try String(contentsOf: url)
             return contents
         } catch {
             print("Error: Couldn't load contents of: \(self)")
-            return ""
+            return nil
         }
     }
 
-    var jsonContentsFromJsonString: [String: Any] {
+    var jsonContentsFromJsonString: [String: Any]? {
         guard let data = data(using: .utf8)
         else {
             print("Error: Couldn't scrape JSON object from string: \(self)")
-            return [:]
+            return nil
         }
         do {
             guard let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else {
                 print("Error: Couldn't parse JSON")
-                return [:]
+                return nil
             }
             return json
         } catch let error as NSError {
             print("Error: Failed to parse JSON: \(error.localizedDescription)")
-            return [:]
+            return nil
         }
     }
 
