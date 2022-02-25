fa937c05408764d5e896cad3d27740a09d86eea6
Ahmed Khalaf <pxlshpr@gmail.com>
2022-02-25 11:45:15 +0500

    bump

---
+++ Sources/SwiftSugar/DataFrame.swift
@@ -0,0 +1,22 @@
+import TabularData
+import Foundation
+
+public extension DataFrame {
+    func write(to url: URL, completion: (() -> Void)? = nil) {
+        do {
+            try writeCSV(to: url)
+            completion?()
+        } catch {
+            print("Error writing CSV: \(error)")
+        }
+    }
+    
+    static func read(from url: URL) -> DataFrame? {
+        do {
+            return try DataFrame(contentsOfCSVFile: url)
+        } catch {
+            print("Error reading CSV: \(error)")
+            return nil
+        }
+    }
+}
