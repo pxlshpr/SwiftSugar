765dff2fa731e5c66420abb6d60596ae4345b6b1
Ahmed Khalaf <pxlshpr@gmail.com>
2022-02-25 10:31:51 +0500

    tag increment

--- Sources/SwiftSugar/Double.swift
+++ Sources/SwiftSugar/Double.swift
@@ -2,6 +2,25 @@ import Foundation
 
 public extension Double {
     var clean: String {
+        /// round it off to a reasonable number first to avoid numbers like `7.00000000000009` resulting in `7.0`
+        let value = self.rounded(toPlaces: 6).truncatingRemainder(dividingBy: 1)
+        if value == 0 {
+            return String(format: "%.0f", self.rounded(toPlaces: 2))
+        } else {
+            return String(self.rounded(toPlaces: 2))
+        }
+    }
+
+    var clean_legacy: String {
        return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
+    }
+
+    var cleanWithoutRounding: String {
+        return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
+    }
+
+    func rounded(toPlaces places: Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
     }
 }
