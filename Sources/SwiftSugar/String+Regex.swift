8aa905fcda1dc320a578cbb83d33b3c1d3c4eb0b
Ahmed Khalaf <pxlshpr@gmail.com>
2022-02-25 10:50:01 +0500

    tag increment

---
+++ Sources/SwiftSugar/String+Regex.swift
@@ -0,0 +1,65 @@
+import Foundation
+
+
+public extension String {
+    func matchesRegex(_ regex: String, caseSensitive: Bool = false) -> Bool {
+        let options: String.CompareOptions = caseSensitive ? [.regularExpression] : [.regularExpression, .caseInsensitive]
+        return range(of: regex, options: options, range: nil, locale: nil) != nil
+    }
+}
+
+public extension String {
+    
+    func extractCapturedGroups(using format: String, caseSensitive: Bool = false) -> [String] {
+        let range = NSRange(
+            startIndex..<endIndex,
+            in: self
+        )
+
+        // Create A NSRegularExpression
+        let capturePattern = format
+        let captureRegex = try! NSRegularExpression(
+            pattern: capturePattern,
+            options: caseSensitive ? [] : [.caseInsensitive]
+        )
+        
+        // Find the matching capture groups
+        let matches = captureRegex.matches(
+            in: self,
+            options: [],
+            range: range
+        )
+
+        guard let match = matches.first else {
+            return []
+        }
+        
+        var captureGroups: [String] = []
+
+        // For each matched range, extract the capture group
+        for rangeIndex in 0..<match.numberOfRanges {
+            let matchRange = match.range(at: rangeIndex)
+            
+            // Ignore matching the entire username string
+            if matchRange == range { continue }
+            
+            // Extract the substring matching the capture group
+            if let substringRange = Range(matchRange, in: self) {
+                let capture = String(self[substringRange])
+                captureGroups.append(capture)
+            }
+        }
+
+        return captureGroups
+    }
+    
+    func extractFirstCapturedGroup(using format: String, caseSensitive: Bool = false) -> String? {
+        return extractCapturedGroups(using: format, caseSensitive: caseSensitive).first
+    }
+    
+    func extractSecondCapturedGroup(using format: String, caseSensitive: Bool = false) -> String? {
+        let groups = extractCapturedGroups(using: format, caseSensitive: caseSensitive)
+        guard groups.count > 1 else { return nil }
+        return groups[1]
+    }
+}
