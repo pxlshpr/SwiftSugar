2ed9f1d515f9f24f7c832883db49272b7ec623dd
Ahmed Khalaf <pxlshpr@gmail.com>
2022-03-02 13:25:36 +0500

    tag increment

--- Sources/SwiftSugar/String/String+Emoji.swift
+++ Sources/SwiftSugar/String/String+Emoji.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-extension String {
+public extension String {
     var isSingleEmoji: Bool { count == 1 && containsEmoji }
 
     var containsEmoji: Bool { contains { $0.isEmoji } }
