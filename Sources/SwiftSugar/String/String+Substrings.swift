3850604207d07315d8c74416f2973d14ff444c03
Ahmed Khalaf <pxlshpr@gmail.com>
2022-03-02 13:40:33 +0500

    tag increment

---
+++ Sources/SwiftSugar/String/String+Substrings.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+extension String {
+    func replacingLastOccurrence(of searchString: String,
+            with replacementString: String,
+            caseInsensitive: Bool = false) -> String
+    {
+        let options: String.CompareOptions
+        if caseInsensitive {
+            options = [.backwards, .caseInsensitive]
+        } else {
+            options = [.backwards]
+        }
+
+        if let range = self.range(of: searchString,
+                options: options,
+                range: nil,
+                locale: nil) {
+
+            return self.replacingCharacters(in: range, with: replacementString)
+        }
+        return self
+    }
+}
