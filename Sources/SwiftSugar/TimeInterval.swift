63cb3b4008ee03554f49fc27a0294d3f654d4b39
Ahmed Khalaf <pxlshpr@gmail.com>
2022-08-28 23:03:33 +0500

    tag increment

--- Sources/SwiftSugar/TimeInterval.swift
+++ Sources/SwiftSugar/TimeInterval.swift
@@ -9,3 +9,29 @@ public extension TimeInterval {
         (Int(self) / 60) % 60
     }
 }
+
+public extension TimeInterval {
+    var seconds: Int {
+        Int(self) % 60
+    }
+
+    var hoursAsDouble: Double {
+        self / 3600.0
+    }
+
+    var stringTime: String {
+        let min = minutes > 1 ? "mins" : "min"
+        if hours != 0 {
+            let hr = hours > 1 ? "hrs" : "hr"
+            if minutes > 0 {
+                return "\(hours) \(hr) \(minutes) \(min)"
+            } else {
+                return "\(hours) \(hr)"
+            }
+        } else if minutes != 0 {
+            return "\(minutes) \(min)"
+        } else {
+            return "<1 min"
+        }
+    }
+}
