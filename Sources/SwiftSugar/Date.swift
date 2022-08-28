945a3007dfe2e9962f76e4730a0facff2329c63e
Ahmed Khalaf <pxlshpr@gmail.com>
2022-08-28 13:57:43 +0500

    tag increment

--- Sources/SwiftSugar/Date.swift
+++ Sources/SwiftSugar/Date.swift
@@ -46,3 +46,102 @@ public extension Date {
     }
 }
 
+public extension Date {
+    
+    static var timestamp: Int64 {
+        Date().timestamp
+    }
+    
+    var timestamp: Int64 {
+        Int64(self.timeIntervalSince1970)
+    }
+    
+    var year: Int {
+        Calendar.current.dateComponents([.year], from: self).year ?? 0
+    }
+
+    var month: Int {
+        Calendar.current.dateComponents([.month], from: self).month ?? 0
+    }
+
+    var day: Int {
+        Calendar.current.dateComponents([.day], from: self).day ?? 0
+    }
+
+    var hour: Int {
+        Calendar.current.dateComponents([.hour], from: self).hour ?? 0
+    }
+
+    var minute: Int {
+        Calendar.current.dateComponents([.minute], from: self).minute ?? 0
+    }
+
+    var second: Int {
+        Calendar.current.dateComponents([.second], from: self).second ?? 0
+    }
+
+    var dayViewTitle: String {
+        if Calendar.current.isDateInToday(self) { return "Today" }
+        if Calendar.current.isDateInYesterday(self) { return "Yesterday" }
+        if Calendar.current.isDateInTomorrow(self) { return "Tomorrow" }
+        
+        let formatter = DateFormatter()
+        let sameYear = year == Date().year
+        formatter.dateFormat = sameYear ? "E, d MMM" : "E, d MMM yy"
+        return formatter.string(from: self)
+    }
+}
+
+public extension Date {
+    func numberOfDaysFrom(_ fromDate: Date) -> Int {
+        let fromDate = fromDate.startOfDay
+        let toDate = startOfDay
+        let numberOfDays = Calendar.current.dateComponents([.day], from: fromDate, to: toDate)
+        return numberOfDays.day ?? 0
+    }
+    
+    func numberOfWeeksFrom(_ fromDate: Date) -> Int {
+//        let numberOfDays = numberOfDaysFrom(fromDate)
+//        return Int(Double(numberOfDays) / 7.0)
+        var calendar = Calendar.autoupdatingCurrent
+        calendar.firstWeekday = 2 /// Start on Monday (or 1 for Sunday)
+        let fromDateWeek = calendar.component(.weekOfYear, from: fromDate.startOfDay)
+        let week = calendar.component(.weekOfYear, from: startOfDay)
+        return week - fromDateWeek
+//        return calendar.dateComponents([.weekOfYear], from: fromDate.startOfDay, to: startOfDay).weekOfYear ?? 0
+    }
+}
+
+public extension Date {
+    func moveDayBy(_ dateIncrement: Int) -> Date {
+        var components = DateComponents()
+        components.day = dateIncrement
+        return Calendar.current.date(byAdding: components, to: self)!
+    }
+}
+
+
+public extension Date {
+    var isToday: Bool {
+        startOfDay == Date().startOfDay
+    }
+    
+    var isYesterday: Bool {
+        startOfDay == Date().startOfDay.addingTimeInterval(-24 * 3600)
+    }
+    
+    var isTomorrow: Bool {
+        startOfDay == Date().startOfDay.addingTimeInterval(24 * 3600)
+    }
+
+    func longDateString(onlyShowYearIfNotCurrent: Bool = false) -> String {
+        let formatter = DateFormatter()
+        if onlyShowYearIfNotCurrent, self.year == Date().year {
+            formatter.dateFormat = "EEEE d MMM"
+        } else {
+            formatter.dateFormat = "EEEE d MMM yyyy"
+        }
+        return formatter.string(from: self)
+    }
+}
+
--- Tests/SwiftSugarTests/SwiftSugarTests.swift
+++ Tests/SwiftSugarTests/SwiftSugarTests.swift
@@ -6,5 +6,36 @@ final class SwiftSugarTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
+        
+        
+    }
+    
+    func matches(for regex: String, in text: String) -> [(string: String, position: Int)] {
+        do {
+            let regex = try NSRegularExpression(pattern: regex)
+            let results = regex.matches(in: text,
+                                        range: NSRange(text.startIndex..., in: text))
+            return results.map {
+                (string: String(text[Range($0.range, in: text)!]),
+                 position: $0.range.lowerBound)
+            }
+        } catch let error {
+            print("invalid regex: \(error.localizedDescription)")
+            return []
+        }
+    }
+    
+    func testRegex() throws {
+        
+//        let string = "🇩🇪€4€9"
+        let string = "€4€9"
+
+        let matched = matches(for: "[0-9]", in: string)
+        
+//        let regex = "[0-9]"
+//        let matched = string.capturedGroups(using: regex)
+        
+        print(matched)
+        // ["4", "9"]
     }
 }
