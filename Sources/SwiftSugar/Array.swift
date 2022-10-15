3115e4d57e5da9fd8f55e6b64c717d062aba4b49
Ahmed Khalaf <pxlshpr@gmail.com>
2022-10-15 16:33:26 +0500

    tag increment

--- Sources/SwiftSugar/Array.swift
+++ Sources/SwiftSugar/Array.swift
@@ -12,4 +12,16 @@ public extension Array where Element: Hashable {
     mutating func removeDuplicates() {
         self = self.removingDuplicates()
     }
+    
+    var mostFrequent: Element? {
+        mostFrequentWithCounts?.element
+    }
+    var mostFrequentWithCounts: (element: Element, count: Int)? {
+        let counts = reduce(into: [:]) { $0[$1, default: 0] += 1 }
+        if let (value, count) = counts.max(by: { $0.1 < $1.1 }) {
+            return (value, count)
+        }
+        // array was empty
+        return nil
+    }
 }
--- Tests/SwiftSugarTests/SwiftSugarTests.swift
+++ Tests/SwiftSugarTests/SwiftSugarTests.swift
@@ -3,42 +3,10 @@ import XCTest
 
 final class SwiftSugarTests: XCTestCase {
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
+        let array = ["a", "b", "a", "c", "a", "b"]
+        XCTAssertEqual(array.mostFrequent, "a")
         
-        let pattern = #"(?:^|[ ]+)([0-9.,]+)"#
-        let text = "Setiap 100g"
-        let groups = text.capturedGroups(using: pattern, allowCapturingEntireString: true)
-        print("We're here")
-    }
-    
-    func matches(for regex: String, in text: String) -> [(string: String, position: Int)] {
-        do {
-            let regex = try NSRegularExpression(pattern: regex)
-            let results = regex.matches(in: text,
-                                        range: NSRange(text.startIndex..., in: text))
-            return results.map {
-                (string: String(text[Range($0.range, in: text)!]),
-                 position: $0.range.lowerBound)
-            }
-        } catch let error {
-            print("invalid regex: \(error.localizedDescription)")
-            return []
-        }
-    }
-    
-    func testRegex() throws {
-        
-//        let string = "🇩🇪€4€9"
-        let string = "€4€9"
-
-        let matched = matches(for: "[0-9]", in: string)
-        
-//        let regex = "[0-9]"
-//        let matched = string.capturedGroups(using: regex)
-        
-        print(matched)
-        // ["4", "9"]
+        let array2 = [1, 1, 2, 1, 2, 1, 2, 2, 1, 1, 1, 2, 2, 2, 2]
+        XCTAssertEqual(array2.mostFrequent, 2)
     }
 }
