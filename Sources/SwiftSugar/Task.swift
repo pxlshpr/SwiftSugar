05c5bfeb1e6c9a363f3e98ac218fa44738944c2a
Ahmed Khalaf <pxlshpr@gmail.com>
2022-10-21 17:06:28 +0500

    tag increment

---
+++ Sources/SwiftSugar/Task.swift
@@ -0,0 +1,22 @@
+#if canImport(SwiftUI)
+import SwiftUI
+
+@available(iOS 16, *)
+public func sleepTask(_ seconds: Int, tolerance: Int = 1) async throws {
+    try await Task.sleep(
+        until: .now + .seconds(seconds),
+        tolerance: .seconds(tolerance),
+        clock: .suspending
+    )
+}
+
+@available(iOS 16, *)
+public func sleepTask(_ seconds: Double, tolerance: Double = 1) async throws {
+    try await Task.sleep(
+        until: .now + .seconds(seconds),
+        tolerance: .seconds(tolerance),
+        clock: .suspending
+    )
+}
+
+#endif
