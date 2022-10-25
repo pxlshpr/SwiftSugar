971376572069d861eaa1e9632c3e81d81d445f1e
Ahmed Khalaf <pxlshpr@gmail.com>
2022-10-25 18:54:45 +0500

    tag increment

--- Sources/SwiftSugar/Sequence.swift
+++ Sources/SwiftSugar/Sequence.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-extension Sequence where Element: Hashable {
+public extension Sequence where Element: Hashable {
 
     /// Returns true if no element is equal to any other element.
     func isDistinct() -> Bool {
