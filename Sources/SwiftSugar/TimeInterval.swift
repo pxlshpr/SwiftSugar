06911f63888b10a5c860407220033e7396e8ef17
Ahmed Khalaf <pxlshpr@gmail.com>
2023-04-08 16:19:13 +0500

    tag increment

--- Sources/SwiftSugar/TimeInterval.swift
+++ Sources/SwiftSugar/TimeInterval.swift
@@ -1,6 +1,10 @@
 import Foundation
 
 public extension TimeInterval {
+    var seconds: Int {
+        Int(self) % 60
+    }
+
     var hours: Int {
         Int(self) / 3600
     }
@@ -8,15 +12,26 @@ public extension TimeInterval {
     var minutes: Int {
         (Int(self) / 60) % 60
     }
+    
+    var hoursAsDouble: Double {
+        self / 3600.0
+    }
+    
+    var milliseconds: Int {
+        return Int((truncatingRemainder(dividingBy: 1)) * 1000)
+    }
 }
 
 public extension TimeInterval {
-    var seconds: Int {
-        Int(self) % 60
-    }
 
-    var hoursAsDouble: Double {
-        self / 3600.0
+    var durationString: String {
+        if hours != 0 {
+            return String(format: "%d:%0.2d:%0.2d", hours, minutes, seconds)
+        } else if minutes != 0 {
+            return String(format: "%d:%0.2d", minutes, seconds)
+        } else {
+            return String(format: "0:%0.2d", seconds)
+        }
     }
 
     //TODO: Rename to description
