4827e8760f6f9886fd2373f0566ca2b0ac0e1f4a
Ahmed Khalaf <pxlshpr@gmail.com>
2023-06-27 12:01:32 +0500

    tag increment

--- Sources/SwiftSugar/Notification.swift
+++ Sources/SwiftSugar/Notification.swift
@@ -9,3 +9,7 @@ public func post(_ names: [Notification.Name]) {
 public func post(_ name: Notification.Name) {
     NotificationCenter.default.post(name: name, object: nil)
 }
+
+public func post(_ name: Notification.Name, userInfo: [AnyHashable : Any]? = nil) {
+    NotificationCenter.default.post(name: name, object: nil, userInfo: userInfo)
+}
